2692388b145b00777df673011c6d0bca5b112fe3
Rafi Mahadika Sujianto <ninkaishou27@gmail.com>
2023-11-14 14:10:03 +0700

    feat: added second screen

--- lib/screens/SecondScreen.dart
+++ lib/screens/SecondScreen.dart
@@ -1,24 +1,31 @@
-import 'package:analytics_test/screens/SecondScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class MainPage extends StatefulWidget {
-  const MainPage({super.key});
+class SecondScreen extends StatefulWidget {
+  const SecondScreen({super.key});
 
   @override
-  State<MainPage> createState() => _MainPageState();
+  State<SecondScreen> createState() => _SecondScreenState();
 }
 
-class _MainPageState extends State<MainPage> {
+class _SecondScreenState extends State<SecondScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Second Screen',
+          style: GoogleFonts.poppins(
+              fontSize: 24, fontWeight: FontWeight.bold, color: Colors.black),
+        ),
+        leading: IconButton(onPressed: () => Navigator.pop(context), icon: const Icon(Icons.arrow_back)),
+      ),
       backgroundColor: Colors.white,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text(
-            'Change to secondary screen',
+            'Change to Third screen',
             style: GoogleFonts.poppins(
                 fontSize: 24, fontWeight: FontWeight.bold, color: Colors.black),
           ),
@@ -29,11 +36,11 @@ class _MainPageState extends State<MainPage> {
               onPressed: () => Navigator.push(
                     context,
                     MaterialPageRoute(
-                      builder: (context) => const SecondScreen(),
+                      builder: (context) => const ThirdScreen(),
                     ),
                   ),
               child: Text(
-                'Change to secondary screen',
+                'Change to third screen',
                 style: GoogleFonts.poppins(
                     fontSize: 12,
                     fontWeight: FontWeight.bold,
